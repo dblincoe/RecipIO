bc0f986480373c89637318bd304b14b8cbc7f781
dblincoe <dblincoe@gmail.com>
2019-04-26 21:48:08 -0400

    Update RecipesWithVotes.sql

--- db_files/RecipesWithVotes.sql
+++ db_files/RecipesWithVotes.sql
@@ -6,7 +6,7 @@ select `r`.`id` AS `id`
 ,`r`.`description` AS `description`
 ,`r`.`time_created` AS `time_created`
 ,`r`.`author_id` AS `author_id`
-,sum((case when (`v`.`vote_value` = 1) then 1 else -(1) end)) AS `vote_score`
+,sum(`v`.`vote_value`) AS `vote_score`
  from (`Recipes` `r` join `RecipeVotes` `v` on((`r`.`id` = `v`.`recipe_id`)))
  GROUP BY r.id, r.title, r.description, r.time_created, r.author_id;
  
