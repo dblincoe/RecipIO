2713f606f2b406968642b5544f32472f5e9b4433
David Blincoe <dblincoe@gmail.com>
2019-04-29 17:39:52 -0400

    Redirec to login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { AuthService } from './auth.service';
 import { MatDialog } from '@angular/material';
 import { RecipeEditorComponent } from './recipe-editor/recipe-editor.component';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-root',
@@ -11,13 +12,17 @@ import { RecipeEditorComponent } from './recipe-editor/recipe-editor.component';
 export class AppComponent {
     title = 'EECS341-Project';
 
-    constructor(private auth: AuthService, private dialog: MatDialog) {}
+    constructor(private auth: AuthService, private dialog: MatDialog, private router: Router) {}
 
     openEditor(): void {
-        const dialogRef = this.dialog.open(RecipeEditorComponent, {
-            height: '750px',
-            width: '750px',
-            data: { recipe: null }
-        });
+        if (this.auth.checkAuth()) {
+            const dialogRef = this.dialog.open(RecipeEditorComponent, {
+                height: '750px',
+                width: '750px',
+                data: { recipe: null }
+            });
+        } else {
+            this.router.navigate([ '/login' ]);
+        }
     }
 }
